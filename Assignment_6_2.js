8e12efb531e2c7930a28d7262c626740a96fb7cf
Kyle Schutter <kmosswork@gmail.com>
2018-05-11 23:00:18 -0600

    Initialized then finished the assignment

---
+++ Assignment_6_2.js
@@ -0,0 +1,26 @@
+var recipeName = "Creamy squash linguine";
+var preperationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log("Recipe Details");
+console.log("--------------");
+
+console.log("Recipe Name:" + recipeName);
+console.log("Preperation Time:" + preperationTime);
+console.log("Recipe By:" + recipeBy);
+console.log("is Vegetarian Dish:" + isVegetarian);
+console.log("Calories:" + calories);
+
+console.log("Recipe details after update");
+console.log("--------------");
+
+preperationTime = 60;
+recipeBy = "Jimmy Cutter George";
+
+console.log("Recipe Name:" + recipeName);
+console.log("Preperation Time:" + preperationTime);
+console.log("Recipe By:" + recipeBy);
+console.log("is Vegetarian Dish:" + isVegetarian);
+console.log("Calories:" + calories);
